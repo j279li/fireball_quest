2ee200276d86125abd1e160bcb71d01136dcb087
Adib Raed <adib.raed3@gmail.com>
2025-12-01 19:07:02 -0500

    Temporary patch for ec2

--- server/main.py
+++ server/main.py
@@ -26,7 +26,8 @@ password_hasher = PasswordHasher()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    # allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
